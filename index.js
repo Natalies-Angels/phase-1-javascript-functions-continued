65093edc0826c82ef3a1b5d456817292ac270947
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-07-20 22:45:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun( activity = "roller-skate")
+{
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathe my dog");
+saturdayFun();
+
+function mondayWork(activity= "go to the office")
+{
+    return(`This Monday, I will ${activity}.`);
+}
+mondayWork("work from home")
+mondayWork();
+
+function wrapAdjective(flair="*")
+{
+  
+   return function innerFunction(adjective="a hard worker")
+    {
+     return `You are ${flair}${adjective}${flair}!`
+    }
+    
+}
+
